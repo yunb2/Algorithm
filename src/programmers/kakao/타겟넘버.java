906f351db7a690669cc27122f9b32096a4588f15
yun <ybchae0801@naver.com>
2020-03-25 15:51:50 +0900

    ADD 타겟넘버

---
+++ src/programmers/kakao/타겟넘버.java
@@ -0,0 +1,23 @@
+package programmers.kakao;
+
+public class 타겟넘버 {
+
+    public int solution(int[] numbers, int target) {
+        return go(0, numbers, target);
+    }
+
+    public int go(int index, int[] numbers, int sum) {
+
+        if(index == numbers.length) {
+            return (sum == 0)? 1 : 0;
+        }
+
+        int cnt = 0;
+
+        cnt += go(index+1, numbers, sum + numbers[index]);
+        cnt += go(index+1, numbers, sum - numbers[index]);
+
+        return cnt;
+    }
+
+}
