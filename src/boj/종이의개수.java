c58be3ac8663259c998a6f2444e714d1426c4578
yun <ybchae0801@naver.com>
2020-03-28 00:45:36 +0900

    MOD 종이의 개수

--- src/boj/종이의개수.java
+++ src/boj/종이의개수.java
@@ -2,9 +2,6 @@ package boj;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class 종이의개수 {
 
@@ -24,47 +21,31 @@ public class 종이의개수 {
             for(int j=0; j<N; ++j) paper[i][j] = Integer.parseInt(str[j]);
         }
 
-        Queue<int[]> papers = new LinkedList<>();
-        papers.add(new int[] {0, 0, N});
-
-        while(!papers.isEmpty()) {
-            int[] info = papers.poll();
-            if(check(info[0], info[1], info[2])) continue;
-            papers.addAll(Arrays.asList(cut(info[0], info[1], info[2])));
-        }
+        go(0, 0, N);
 
         for(int a : answer) System.out.println(a);
-
     }
 
-    public static boolean check(int r, int c, int n) {
+    public static void go(int sr, int sc, int n) {
 
-        int value = paper[r][c];
+        int value = paper[sr][sc];
 
-        for(int i=r; i<r+n; ++i) {
-            for(int j=c; j<c+n; ++j) {
-                if(paper[i][j] != value) return false;
+        for(int r=sr; r<sr+n; ++r) {
+            for(int c=sc; c<sc+n; ++c) {
+
+                if(paper[r][c] == value) continue;
+
+                for(int nr=sr; nr<sr+n; nr+=n/3) {
+                    for(int nc=sc; nc<sc+n; nc+=n/3) {
+                        go(nr, nc, n/3);
+                    }
+                }
+
+                return;
             }
         }
 
         answer[value+1]++;
-        return true;
-    }
-
-    public static int[][] cut(int r, int c, int n) {
-
-        int[][] papers = new int[9][3];
-        int index = 0;
-
-        for(int i=r; i<r+n; i+=n/3) {
-            for(int j=c; j<c+n; j+=n/3) {
-                papers[index][0] = i;
-                papers[index][1] = j;
-                papers[index++][2] = n/3;
-            }
-        }
-
-        return papers;
     }
 
 }
