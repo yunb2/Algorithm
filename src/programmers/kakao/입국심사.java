41fb7704f70f51e3e62bfb51ad8b8707e8abdc67
yun <ybchae0801@naver.com>
2020-03-30 19:39:13 +0900

    ADD 입국심사

---
+++ src/programmers/kakao/입국심사.java
@@ -0,0 +1,42 @@
+package programmers.kakao;
+
+import java.util.Arrays;
+
+public class 입국심사 {
+
+    public static void main(String[] args) {
+        System.out.println(solution(6, new int[] {7, 10}));
+    }
+
+    public static long solution(int n, int[] times) {
+
+        long answer = 0;
+
+        Arrays.sort(times);
+
+        long start = 0;
+        long end = (long) times[times.length-1] * (long) n;
+
+        while(start <= end) {
+
+            long mid = (start + end) / 2;
+            long sum = 0;
+
+            for(int t : times) {
+                if(t > mid) break;
+                sum += mid / t;
+            }
+
+            if(sum < n) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+                answer = mid;
+            }
+
+        }
+
+        return answer;
+    }
+
+}
