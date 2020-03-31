e1f7d1056699a0ebafe867b363c32cfa72ad80a5
yun <ybchae0801@naver.com>
2020-03-31 20:43:46 +0900

    MOD 호텔방배정 TreeSet

--- src/programmers/kakao/호텔방배정.java
+++ src/programmers/kakao/호텔방배정.java
@@ -1,7 +1,9 @@
 package programmers.kakao;
 
 import java.util.Arrays;
-import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
 class 호텔방배정 {
 
@@ -9,28 +11,29 @@ class 호텔방배정 {
         System.out.println(Arrays.toString(solution(10, new long[]{1, 3, 4, 1, 3, 1})));
     }
 
-    static HashMap<Long, Long> rooms;
+    static Set<Long> rooms;
+    static TreeSet<Long> nextRooms;
 
     public static long[] solution(long k, long[] room_number) {
 
-        rooms = new HashMap<>();
+        rooms = new HashSet<>();
+        nextRooms = new TreeSet<>();
 
         return Arrays.stream(room_number)
-                        .map(호텔방배정::findRoom)
-                        .toArray();
+                    .map(호텔방배정::findRoom)
+                    .toArray();
     }
 
     public static long findRoom(long num) {
 
-        if(!rooms.containsKey(num)) {
-            rooms.put(num, rooms.getOrDefault(num+1, num+1));
-            return num;
-        }
+        if(rooms.contains(num)) return findRoom(nextRooms.higher(num));
 
-        long next = findRoom(rooms.get(num));
+        rooms.add(num);
+        nextRooms.remove(num);
 
-        rooms.put(num, next);
-        return next;
+        if(!rooms.contains(num+1)) nextRooms.add(num+1);
+
+        return num;
     }
 
 }
