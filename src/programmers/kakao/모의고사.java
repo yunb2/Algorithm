7b385073fd151a31469767e38c52f1e7728edd17
yun <ybchae0801@naver.com>
2020-04-04 15:55:11 +0900

    ADD 모의고사

---
+++ src/programmers/kakao/모의고사.java
@@ -0,0 +1,32 @@
+package programmers.kakao;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class 모의고사 {
+
+    public int[] solution(int[] answers) {
+
+        int[] s1 = {1,2,3,4,5};
+        int[] s2 = {2,1,2,3,2,4,2,5};
+        int[] s3 = {3,3,1,1,2,2,4,4,5,5};
+
+        int[] score = new int[3];
+
+        for(int i=0; i<answers.length; ++i) {
+            if(answers[i] == s1[i % s1.length]) score[0]++;
+            if(answers[i] == s2[i % s2.length]) score[1]++;
+            if(answers[i] == s3[i % s3.length]) score[2]++;
+        }
+
+        int max = Arrays.stream(score).max().getAsInt();
+
+        List<Integer> list = new LinkedList<>();
+
+        for(int i=0; i<score.length; ++i) if(score[i] == max) list.add(i+1);
+
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+
+}
